cf99171f3d119317815457bc6a545002fb7cdff9
petergillhub <petergillhmg@gmail.com>
2024-05-01 22:31:41 +0100

    updated

--- 03_conversions_operations.js
+++ 03_conversions_operations.js
@@ -25,3 +25,33 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 // 1 => true ; 0 => false
 // "" => false
 //  "peter" => true 
+
+// ***************************** Operations ***************************************************
+
+
+let value = 4 
+let negvalue = -value
+
+// console.log(negvalue) // -4
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2)
+
+let str1 = "Peter"
+let str2 = " Gill"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter); // 101    
+
+
