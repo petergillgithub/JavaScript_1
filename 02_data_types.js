a43f16030981e4be0e77dc12fa19aef26c33fd3c
petergillhub <petergillhmg@gmail.com>
2024-05-01 19:35:40 +0100

    data types

---
+++ 02_data_types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js Code as newer Version
+
+let age = 18 
+let isLoggedIn = true
+
+
+
+
+/*
+number = 1,2,3,4
+string = ""
+boolean = True/False
+null = standalone value
+undefined = 
+symbol =  unique   
+
+
+
+*/
+
+console.log(typeof null); //object
+
+console.log(typeof undefined);  //undefined
