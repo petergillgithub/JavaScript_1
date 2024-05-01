dfc21a6d5f9464cd39a50905fbdc28cc94792504
petergillhub <petergillhmg@gmail.com>
2024-05-01 22:10:12 +0100

    conversation

---
+++ 03_conversions_operations.js
@@ -0,0 +1,27 @@
+let score = "20"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+
+let valueNumber = Number(score) // even it is string it will convert to Number
+// console.log(typeof valueNumber)
+
+
+// "33" = 33
+
+// "33abc" = NaN
+
+// true = 1 ; false = = 0 ; 
+
+
+
+let isLoggedIn = "jjj"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanisLoggedIn);
+
+
+// 1 => true ; 0 => false
+// "" => false
+//  "peter" => true 
