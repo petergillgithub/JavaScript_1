738c931d3ba52bc783108ced583663c8fc3a96cf
petergillhub <petergillhmg@gmail.com>
2024-05-03 16:37:54 +0100

    math and number

---
+++ 06_num_math.js
@@ -0,0 +1,27 @@
+const score = 400
+
+// console.log(score);
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber= 23.899
+
+// console.log(otherNumber.toPrecision(2));
+
+
+// ++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.0))
+console.log(Math.floor(4.1))
+console.log(Math.min(4,3,4,2,5,6))
+
+
+
